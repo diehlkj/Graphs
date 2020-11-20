7ad886ddb72501140e9ae1cd079fe3aa59216d29
Ken Diehl <diehlkj.99@gmail.com>
2020-11-19 23:13:47 -0500

    began building class and methods for discovery of room connections and traversal

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -10,11 +10,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# ? map_file = "maps/test_line.txt"
+map_file = "maps/test_cross.txt"
+# ? map_file = "maps/test_loop.txt"
+# ? map_file = "maps/test_loop_fork.txt"
+# ! map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -26,8 +26,39 @@ world.print_rooms()
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
-# traversal_path = ['n', 'n']
+# traversal_path = ['n', 'n', 's', 's', 'w', 'w', 'e', 'e', 'e', 'e', 'w', 'w', 's', 's']
 traversal_path = []
+
+# * Code Project Here
+class AllRooms:
+    def __init__(self):
+        self.room = {}
+    
+    # * 
+    def add_room(self, room_id):
+        if room_id not in self.room:
+            # ? Creates Key for Room ID
+            self.room[room_id] = {}
+            
+            # ? Populate Room ID's possible exits as 'cardnial': None (until explored)
+            for cardinal in player.current_room.get_exits():
+                self.room[room_id][cardinal] = None
+    
+    def update_exit(self, room_id, cardinal, connection_id):
+        if self.room[room_id][cardinal] is None:
+            self.room[room_id][cardinal] = connection_id
+    
+    # * DFT until no exits other than previous room
+    # * Append the direction moved to traversal path each time
+    def dft(self, room_id):
+        pass
+    
+    # * BFT back to first room with an unexplored exit
+    # ? For the traversl paths, check previous graph project for how to return the path taken from starting node to target. Use this to append the whole path taken instead of one move at a time.
+    # ? The 'target' of the BFT will be the first room found with a 'None' value on a cardinal key
+    def bft(self, room_id):
+        pass
+            
 
 
 
@@ -47,16 +78,22 @@ else:
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
-
-#######
-# UNCOMMENT TO WALK AROUND
-#######
 player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+print(player.current_room.id)
+print(player.current_room.get_exits())
+# ? player.travel('n', True)
+player.travel('n', True)
+
+
+#######!
+#! UNCOMMENT TO WALK AROUND
+#######!
+
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
